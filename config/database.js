64abdcc94ba93467bcea00983fd77284a67e7b81
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2016-10-23 14:12:56 +0200

    db can be dropped before launch

--- config/database.js
+++ config/database.js
@@ -5,11 +5,17 @@ new Sqlite.Database('data');
 
 var exports = module.exports = {};
 
-exports.dataBase = new Sequelize('db', '', '',
-    {
-        dialect: 'sqlite',
-        storage: 'data.sqlite',
-        define: {
-            timestamps: false
-        }
-    });
+var sequelize =
+    new Sequelize('db', '', '',
+        {
+            dialect: 'sqlite',
+            storage: 'data.sqlite',
+            define: {
+                timestamps: false
+            }
+        });
+
+// uncomment if db should be dropped on launch
+//sequelize.sync({force: true});
+
+exports.dataBase = sequelize;
