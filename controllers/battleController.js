8f44fd39394c426b0ee9abef85e63113e1e65dbd
DominicSina <dominic.sina@gmx.ch>
2016-11-26 02:04:10 +0100

    tweaked /battles/query route. Added battle/{id}/pots that sums up all bets per trainer for a specific battle

--- controllers/battleController.js
+++ controllers/battleController.js
@@ -7,6 +7,10 @@ var exports = module.exports = {};
 exports.getBattles = function (request, reply) {
     var queryString = request.params.query_string;
     var pairs = queryString.split("&");
+
+    var id = pairs.find(function (str) {
+        return str.startsWith("id")
+    });
     var lim = pairs.find(function (str) {
         return str.startsWith("limit")
     });
@@ -17,8 +21,51 @@ exports.getBattles = function (request, reply) {
         return str.startsWith("is_finished")
     });
 
-    battleClient.getBattles({limit: lim.split("=")[1], offset: offs.split("=")[1], is_finished: isF.split("=")[1]})
+    var obj={};
+    if(typeof id!=="undefined")
+        obj["id"]=id.split("=")[1];
+    if(typeof lim!=="undefined")
+        obj["limit"]=lim.split("=")[1];
+    if(typeof offs!=="undefined")
+        obj["offset"]=offs.split("=")[1];
+    if(typeof isF!=="undefined")
+        obj["is_finished"]=isF.split("=")[1];
+
+    //battleClient.getBattles({limit: lim.split("=")[1], offset: offs.split("=")[1], is_finished: isF.split("=")[1]})
+    battleClient.getBattles(obj)
         .end(function (response) {
             reply(response.body).code(200);
+        })
+};
+
+exports.getPots = function (request, reply) {
+    var battleId = request.params.id;
+
+    battleClient.getBattles({id: battleId})
+        .end(function (response) {
+            var tId1=response.body.team1.trainer.id;
+            var tId2=response.body.team2.trainer.id;
+
+            //TODO: should get bets from db
+            var bets=[
+                {id:0,battleId:battleId,trainerId:tId1,amount:100},
+                {id:1,battleId:battleId,trainerId:tId2,amount:20},
+                {id:2,battleId:battleId,trainerId:tId2,amount:50},
+                {id:3,battleId:battleId,trainerId:tId1,amount:200},
+                {id:4,battleId:battleId,trainerId:tId1,amount:80}];
+
+            var pot1=0;
+            var pot2=0;
+
+            bets.forEach(function(bet){
+                if(bet.trainerId==tId1){
+                    pot1+=bet.amount;
+                }
+                else if(bet.trainerId==tId2){
+                    pot2+=bet.amount;
+                }
+            });
+
+            reply([{trainerId: tId1, pot: pot1},{trainerId: tId2, pot: pot2}]).code(200);
         })
 };
--- routes/battleRoutes.js
+++ routes/battleRoutes.js
@@ -17,9 +17,29 @@ var getBattlesConfig = new RouteConfigBuilder()
         }
     }).build();
 
+var getPotsConfig = new RouteConfigBuilder()
+    .setDescription('Get Pots for a battle')
+    .setAuth(false)
+    .setParams({
+        id: Joi.number().integer().positive()
+    })
+    .setResponses({
+        200: {
+            description: 'Success',
+            schema: BattleSchemata.battlePots
+        }
+    }).build();
+
 server.route({
     method: 'GET',
     path: '/battles/{query_string}',
     handler: BattleController.getBattles,
     config: getBattlesConfig
+});
+
+server.route({
+    method: 'GET',
+    path: '/battle/{id}/pots',
+    handler: BattleController.getPots,
+    config: getPotsConfig
 });
