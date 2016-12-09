ae01cf5212e5aaf68cb8b9fa0fa4d3c4f9ce418d
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2016-12-09 23:45:18 +0100

    get transactions route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user').User;
+const Transaction = require('../models/transaction').Transaction;
 const _ = require('underscore');
 const auth = require('../config/auth');
 
@@ -12,31 +13,32 @@ exports.getUsers = function (request, reply) {
                 id: user.id,
                 username: user.username,
                 email: user.email,
-                balance: user.balance}
+                balance: user.balance
+            }
         });
         reply(users).code(200);
     })
 };
 
-function getUserIdFromRequest(request){
+function getUserIdFromRequest(request) {
     return request.auth.credentials.sub;
 }
 
-exports.getUserId = function(request, reply){
-    User.findById(request.params.id).then(function(user){
+exports.getUserId = function (request, reply) {
+    User.findById(request.params.id).then(function (user) {
         reply(user).code(200);
     })
 };
 
-exports.getAuthenticatedUser = function(request, callback){
+exports.getAuthenticatedUser = function (request, callback) {
     var userId = getUserIdFromRequest(request);
-    User.findById(userId).then(function(user){
+    User.findById(userId).then(function (user) {
         callback(user);
     })
 };
 
-exports.replyWithAuthenticatedUser = function(request, reply){
-    exports.getAuthenticatedUser(request, function(user){
+exports.replyWithAuthenticatedUser = function (request, reply) {
+    exports.getAuthenticatedUser(request, function (user) {
         user = {
             id: user.id,
             username: user.username,
@@ -47,10 +49,10 @@ exports.replyWithAuthenticatedUser = function(request, reply){
     });
 };
 
-exports.updateUser = function(request, reply){
-    User.findById(request.params.id).then(function(user){
-       for(var attrName in request.payload){
-            if(attrName === 'password'){
+exports.updateUser = function (request, reply) {
+    User.findById(request.params.id).then(function (user) {
+        for (var attrName in request.payload) {
+            if (attrName === 'password') {
                 var hashPassword = auth.createHash(request.payload[attrName]);
                 user[attrName] = hashPassword;
             } else {
@@ -58,29 +60,29 @@ exports.updateUser = function(request, reply){
             }
         }
         user.save();
-        
+
         return reply(user).code(200);
     })
 };
 
 exports.saveUser = function (request, reply) {
-    
+
     User.create({
         username: request.payload.username,
         password: auth.createHash(request.payload.password),
         email: request.payload.email,
         balance: request.payload.balance
     })
-    
-    .then(function (user) {
-        user = {
-            id: user.id,
-            username: user.username,
-            email: user.email,
-            balance: user.balance
-        };
-        reply(user).code(201);
-    })
+
+        .then(function (user) {
+            user = {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                balance: user.balance
+            };
+            reply(user).code(201);
+        })
 };
 
 exports.deposit = function (request, reply) {
@@ -109,5 +111,23 @@ exports.withdraw = function (request, reply) {
         }).catch(error => {
             reply().code(500);
         });
+    });
+};
+
+exports.getTransactions = function (request, reply) {
+    var userId = getUserIdFromRequest(request);
+    var requestedUserId = request.params.id;
+
+    if (userId != requestedUserId) {
+        reply('Cannot request transactions of different user.').code(403);
+        return;
+    }
+
+    Transaction.find({
+        where: {
+            user_id: userId
+        }
+    }).then(function (transactions) {
+        reply(transactions).code(200);
     });
 };
