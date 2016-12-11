2ff2e00ba36da61beebe259c64879a269bbfbe39
DominicSina <dominic.sina@gmx.ch>
2016-12-11 22:23:35 +0100

    added status attribute to bet schema

--- models/bet.js
+++ models/bet.js
@@ -22,9 +22,22 @@ var Bet = dataBase.define('bet', {
         type:Sequelize.INTEGER,
         defaultValue: 0,
         allowNull: false
+    },
+
+    status: {
+        type:Sequelize.ENUM,
+        values:['running','won','lost'],
+        defaultValue: 'running',
+        allowNull: false
     }
 });
 
 Bet.sync();
+
+Bet.build({battleId: 1, userId:1});
+Bet.build({battleId: 2, userId:1});
+Bet.build({battleId: 3, userId:1});
+Bet.build({battleId: 4, userId:2});
+
 
 exports.Bet = Bet;
